990409f02e798d5279fcae9c4847fc9b9588587e
Erica Moszkowski <erica.moszkowski@gmail.com>
2017-10-08 14:00:27 -0400

    add matlab latextable to list of things to clone in setup.m

--- setup.sh
+++ setup.sh
@@ -6,6 +6,95 @@
 SCRIPTNAME=$(basename $0)
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+### Todo: download brew if on mac and I don't have it already
+
+# define contains function; this is important for later
+# usage: contains aList anItem
+contains() {
+    [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && exit(0) || exit(1)
+}
+
+# OSX specific installs
+case $OSTYPE in
+    darwin*) # OS X
+
+	# install brew and other useful things
+	if [ ! "$(which brew)" == "/usr/local/bin/brew" ];
+	then
+	    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+	    brew install wget
+	    brew install rename
+	fi
+
+	emacs_version="$(emacs --version | head -n 1 | awk '{print $NF}')"
+	bad_emacs_version="22."
+	if [ emacs_version == *"$bad_emacs_version"* ];
+	then
+	    # remove old version of emacs
+	    sudo rm /usr/bin/emacs
+	    sudo rm -rf /usr/share/emacs
+
+	    # install new version of emacs
+	    brew cask install emacs
+	fi
+
+
+	casks="$(brew cask list)"
+
+	# LaTeX and LaTeX software
+	if [ ! contains casks mactex ];
+	then
+	    brew cask install mactex
+	fi
+	if [ ! contains casks lyx ];
+	then
+	   brew cask install lyx
+	fi
+
+	# Dropbox
+	if [ ! contains casks dropbox ]
+	then
+	    brew cask install dropbox
+	fi
+
+	# Julia
+	if [ ! contains casks julia ]
+	then
+	    brew cask install julia
+	fi
+
+	# R
+	if brew ls --versions r > /dev/null; then
+	    # The package is installed
+	else
+	    # The package is not installed
+	    brew install r
+	fi
+
+	if [ ! contains casks rstudio ]
+	then
+	    brew cask install rstudio
+	fi
+
+	# Meld
+	if [ ! contains casks meld ]
+	then
+	    brew cask install caskroom/cask/meld
+	fi
+
+
+	# Tmux
+	if brew ls --versions tmux > /dev/null; then
+	    # The package is installed
+	else
+	    # The package is not installed
+	    brew install tmux
+	fi
+
+
+	;;
+esac
 
 ### Emacs setup
 
@@ -37,6 +126,23 @@ then
      echo "$SCRIPTNAME: julia-repl minor mode already installed"
 fi
 
+# Setup matlab mode
+if [ ! -d ~/.emacsconfig/matlab-mode ];
+then
+    mkdir -p ~/.emacsconfig/matlab-mode
+    git clone git://git.code.sf.net/p/matlab-emacs/src matlab-mode
+else
+    echo "$SCRIPTNAME: matlab-mode already installed"
+fi
+
+# Get MATLAB packages I like
+if [ ! -d ~/.matlabconfig/latexTable];
+then
+    mkdir -p ~/.matlabconfig/latexTable
+    git clone https://github.com/eliduenisch/latexTable.git
+else
+    echo "$SCRIPTNAME: matlab latexTable already installed"
+fi
 
 # ESS setup
 
@@ -90,7 +196,8 @@ for FILE in .bashrc .emacs .tmux.conf .gitconfig .gitignore_global;
 do
     if [ ! -h "$HOME/$FILE" ];
     then
-        ln --symbolic --target-directory $HOME $SCRIPTDIR/$FILE
+        # ln --symbolic --target-directory $HOME $SCRIPTDIR/$FILE
+	ln -s $SCRIPTDIR/$FILE $HOME
     else
         echo "$SCRIPTNAME: $FILE already linked"
     fi
